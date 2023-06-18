9bd4b6a47aeccb0c01925c3fe9c6ed69a84a0cbd
HAMBALI <117418803+HAMBALI251298@users.noreply.github.com>
2023-06-18 10:51:52 +0700

    misc.js

---
+++ misc.js
@@ -0,0 +1,7 @@
+
+
+/**
+ * Gets the short version of a DateTime object.
+ * @param {*} dateObj
+ */
+const getShortDate = (dateObj) => `${dateObj.getDay()}/${dateObj.getMonth()}/${dateObj.getFullYear()}`;￼Enter
